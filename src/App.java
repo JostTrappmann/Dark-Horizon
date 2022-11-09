d2cfe4e4c2bea33bfd7093f2f8f798cd57faf50e
JostTrappmann <94616625+JostTrappmann@users.noreply.github.com>
2022-11-09 08:29:00 +0100

    Begin

--- src/App.java
+++ src/App.java
@@ -1,14 +1,19 @@
 import Armor.Armor;
+import Elements.Earth;
 import Weapon.Weapon;
 import Monster.*;
 public class App {
     public static void main(String[] args){
         Slime slime = new Slime();
         GUI gui = new GUI();
-        gui.fight(slime);
-        Menue menue = new Menue();
-        Player player = menue.createPlayer();
-        menue.printPlayer(player);
+        //Menue menue = new Menue();
+        //Player player = menue.createPlayer();
+        gui.fight(slime, slime.getMaxHealth(), new Player("Zyklopus", new Earth()));
+
+
+
+
+        //menue.printPlayer(player);
         System.out.println("");
     }
 }
--- src/GUI.java
+++ src/GUI.java
@@ -1,16 +1,21 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import Monster.*;
 
-public class GUI {
+public class GUI implements ActionListener {
     private JFrame frame = new JFrame();
     private JPanel background = new JPanel();
+    private String action = "ddddd";
 
     public GUI(){
 
 
         frame.setSize(1200,675);
         frame.setResizable(false);
+        frame.setTitle("Dark Horizon");
 
         background.setBounds(0,0,1200,675);
         background.setAlignmentX(0);
@@ -26,17 +31,18 @@ public class GUI {
 
 
     }
-    public void fight(Monster monster,Player player){
+    public void fight(Monster monster,int hp ,Player player){
         JButton attack = new JButton();
         JButton items = new JButton();
         JButton element = new JButton();
         JLabel label = new JLabel();
+        String a;
+
         attack.setText("Attack");
-
-
         attack.setForeground(Color.WHITE);
         attack.setBackground(Color.BLACK);
         attack.setBounds(350, 500,500,50 );
+        attack.addActionListener(this);
         attack.setActionCommand("ATTACK");
         background.add(attack, 0);
 
@@ -44,28 +50,39 @@ public class GUI {
         items.setForeground(Color.WHITE);
         items.setBackground(Color.BLACK);
         items.setBounds(75, 575, 500,50 );
-        items.setActionCommand("ITEMS");
+        items.addActionListener(this);
         background.add(items, 0);
 
         element.setText("Element");
         element.setForeground(Color.WHITE);
         element.setBackground(Color.BLACK);
         element.setBounds(625, 575,500,50 );
-        element.setActionCommand("ELEMENT");
+        element.addActionListener(this);
         background.add(element, 0);
 
         frame.setIconImage(monster.getImageIcon().getImage());
-        label.setBounds(450, 100, 300,300);
+        label.setBounds(450, 100, 350,350);
         label.setIcon(monster.getImageIcon());
         label.setBackground(Color.BLACK);
+        label.setForeground(Color.white);
         background.add(label, 0);
-
 
 
         label.setVisible(true);
         attack.setVisible(true);
         items.setVisible(true);
         element.setVisible(true);
+
+
+
+
     }
 
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        switch(e.getActionCommand()){
+            case "ATTACK": action = "attack";
+            default:
+        }
+    }
 }
